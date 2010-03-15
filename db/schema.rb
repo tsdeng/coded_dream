fb19dd375f9c9c8060b700ab4c661d74635114d2
tsdeng <tsdeng@thoughtworks.com>
2010-03-15 20:30:26 +0800

    removed unnessary file

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100102162806) do
+ActiveRecord::Schema.define(:version => 20100102172837) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
