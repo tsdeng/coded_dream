8f9d6b218143dba2d7837e46a3897a17c3dbd985
tsdeng <tsdeng@thoughtworks.com>
2010-08-15 09:46:20 +0100

    remove bad function..

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :articles,:foreign_key => :author_id
   def self.login(username,password,session)
     current_user = self.find_by_username_and_password(username,password)
-     session[:user_id]=current_user.id
+     if current_user then session[:user_id]=current_user.id end
   end
   validates_length_of :password, :within => 6..10
   validates_length_of :username, :within => 6..20
